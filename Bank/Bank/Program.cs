11fe3a813a0dc43f169ee08d7e954c19b316a7ae
otabekravshanov0527 <otabekravshanov0527@gmail.com>
2026-09-25 22:19:05 +0700

    #2 Добавлена функция отображения истории транзакций

--- Bank/Bank/Program.cs
+++ Bank/Bank/Program.cs
@@ -16,7 +16,7 @@
             account1.MakeWithdrawal(123, DateTime.UtcNow,";)");
             Console.WriteLine($"Balance:{account1.Balance}");
 
-            Console.WriteLine(account1.GetAccountHistory( ));
+            Console.WriteLine(account1.GetAccountHistory());
 
             try
             {
